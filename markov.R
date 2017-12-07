67ae22b05d3767c3c1701f4b6bb1fc4d73ecd3aa
pnicewicz421 <peter.nicewicz@gmail.com>
2017-12-07 07:46:18 -0500

    Add files via upload

--- markov.R
+++ markov.R
@@ -1,23 +1,40 @@
 library(varhandle)
+library(stringr)
 library(dplyr)
 
 # Markov models
 # bigram
 # provide word
-history <- readline(prompt="Enter one word: ")
+NumberOfWords <- function(samp) {
+    samp <- str_trim(gsub("\\s+", " ", samp))
+    sps <- gregexpr(" ", samp)[[1]]
+    csps <- length(sps[which(sps>0)]) + 1
+    csps
+}
 
+
+history <- readline(prompt="Enter words (two or one): ")
 history <- paste0(str_trim(history), " ")
 
 #check 
 #bigrams <- ngram(sample, 2)
 
 srchstr <- paste0("^", history)
+
+#how many words?
+numwords <- NumberOfWords(history)
+
+
+
 pickin <- grep(srchstr, twowords, useBytes=TRUE)
+
+
+
 universe <- twowords[pickin]
 
 denominator <- length(universe)
 
-thewords <- substr(universe, nchar(history)+1, stop=nchar(universe)) 
+thewords <- substr(universe, nchar(history) + 1, stop=nchar(universe)) 
 
 words <- as.data.frame(sort(table(thewords), decreasing=TRUE))
 words <- transform(words, prob=Freq / denominator)
