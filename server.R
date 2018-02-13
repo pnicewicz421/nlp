915b673b19817ccd5ea91b01ec60939b7a17e75b
pnicewicz421 <peter.nicewicz@gmail.com>
2018-02-13 13:49:52 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -5,24 +5,25 @@ library(shiny)
 
 
 shinyServer(function(input, output) {
-    rv <- reactiveValues()
-    rv$setupComplete <- FALSE
+  rv <- reactiveValues()
+  rv$setupComplete <- FALSE
+  
+  ## simulate data load
+  observe({
+    source('predictword.R')
+    rv$setupComplete <- TRUE    
     
-    ## simulate data load
-    observe({
-        source('predictword.R')
-        rv$setupComplete <- TRUE    
-
-        ## the conditional panel reads this output
-        output$setupComplete <- reactive({
-            return(rv$setupComplete)
-        })
-        outputOptions(output, 'setupComplete', suspendWhenHidden=FALSE)
-        
+    ## the conditional panel reads this output
+    output$setupComplete <- reactive({
+      return(rv$setupComplete)
     })
+    outputOptions(output, 'setupComplete', suspendWhenHidden=FALSE)
     
-    observeEvent(input$submit, {
-        answer <- process_gram(phrase)
-        output$nextWord <- renderText({ answer })
-    })
+  })
+  
+  observeEvent(input$submit, {
+    phrase <- input$phrase
+    answer <- process_gram(phrase)
+    output$nextWord <- renderText({ answer })
+  })
 })
--- ui.R
+++ ui.R
@@ -3,15 +3,15 @@ library(shinydashboard)
 library(shinyjs)
 
 shinyUI(fluidPage(
-    dashboardHeader(),
-    dashboardSidebar(),
-    dashboardBody(
-        conditionalPanel(condition = "output.setupComplete",
-                         textInput("phrase", "Enter Phrase", ""),
-                         actionButton("submit", "Enter"),
-                         textOutput("nextWord")
-        ),
-        conditionalPanel(condition = "!output.setupComplete",
-                         box( title = "Loading, Please Wait."))
-    )))
-
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody(
+    conditionalPanel(condition = "output.setupComplete",
+                     textInput("phrase", "Enter Phrase", ""),
+                     actionButton("submit", "Enter"),
+                     textOutput("nextWord")
+    ),
+    conditionalPanel(condition = "!output.setupComplete",
+                   box( title = "Loading, Please Wait.", background="olive"))
+                     
+  )))
